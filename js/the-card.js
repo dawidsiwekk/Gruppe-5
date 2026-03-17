3b2f19aee757d47ebb538de8643226aedd6d1497
dawidsiwekk <dawidsiwekk@gmail.com>
2026-03-17 10:24:28 +0100

    hover function added (still need tweaking)

--- js/the-card.js
+++ js/the-card.js
@@ -1,2 +1,22 @@
 "use strict";
 
+// This function is making the location appear while hovering on fish
+const fish1 = document.getElementsByClassName("fish-box-1");
+const fish2 = document.getElementsByClassName("fish-box-2");
+
+const area1 = document.getElementById("area-1");
+const area2 = document.getElementById("area-2");
+
+function setupHover(fish, area) {
+    fish.addEventListener("mouseenter", () => {
+        area.style.display = "block";
+    });
+
+    fish.addEventListener("mouseleave", () => {
+    area.style.display = "none";
+    });
+
+}
+
+setupHover(fish1, area1);
+setupHover(fish2, area2);
