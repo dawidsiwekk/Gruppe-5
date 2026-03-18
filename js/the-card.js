d406338332771ccf0dfa087f2ea94facf9b0708c
dawidsiwekk <dawidsiwekk@gmail.com>
2026-03-18 10:45:02 +0100

    added last location

--- js/the-card.js
+++ js/the-card.js
@@ -3,9 +3,13 @@
 // This function is making the location appear while hovering on fish
 const fish1 = document.querySelector(".fish-box-1");
 const fish2 = document.querySelector(".fish-box-2");
+const fish3 = document.querySelector(".fish-box-3");
+const fish4 = document.querySelector(".fish-box-4");
 
 const area1 = document.getElementById("area-1");
 const area2 = document.getElementById("area-2");
+const area3 = document.getElementById("area-3");
+const area4 = document.getElementById("area-4");
 
 function setupHover(fish, area) {
     fish.addEventListener("mouseenter", () => {
@@ -20,3 +24,5 @@ function setupHover(fish, area) {
 
 setupHover(fish1, area1);
 setupHover(fish2, area2);
+setupHover(fish3, area3);
+setupHover(fish4, area4);
